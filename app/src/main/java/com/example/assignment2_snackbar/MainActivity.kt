1923ca93426bda60c972c6f6b1f334a500e6af73
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-01-29 00:31:41 -0500

    implemented snackbar

--- app/src/main/java/com/example/assignment2_snackbar/MainActivity.kt
+++ app/src/main/java/com/example/assignment2_snackbar/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,16 +20,16 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
 
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
+            Snackbar.make(
+                view,
                 R.string.correct_toast,
-                Toast.LENGTH_SHORT
+                Snackbar.LENGTH_SHORT
             ).show()
         }
 
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
+            Snackbar.make(
+                view,
                 R.string.incorrect_toast,
                 Toast.LENGTH_SHORT
             ).show()
